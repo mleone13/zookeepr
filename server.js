6f45df04c4db57c9b57010f1105071ca8305643b
Happy Gilmore <maryleone@rocketcentral.com>
2022-06-20 21:50:32 -0400

    front end added

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const PORT = process.env.PORT || 3001;
 const app = express ();
 
 
+app.use(express.static('public'));
+
 //parse incoming string or array data
 app.use(express.urlencoded({extended:true}));
 //parse incoming JSON data
 app.use(express.json());
+
+
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -101,6 +105,23 @@ app.get('/api/animals/:id', (req,res) => {
         res.send(404);
     }
 });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.post('/api/animals', (req,res) => {
     //set id based on what the next index of the array will be

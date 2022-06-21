d6d1be22c9bcf34872cd5110e96930fe88875355
Happy Gilmore <maryleone@rocketcentral.com>
2022-06-20 21:00:02 -0400

    Insomia

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs =require ('fs');
+const path = require('path');
 const {animals} = require ('./data/animals.json');
 const express = require ('express');
 const QueryString = require('qs');
@@ -5,10 +7,15 @@ const PORT = process.env.PORT || 3001;
 const app = express ();
 
 
-function filterByQuery(query, animlasArray) {
+//parse incoming string or array data
+app.use(express.urlencoded({extended:true}));
+//parse incoming JSON data
+app.use(express.json());
+
+function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
-    let filteredResults = animlasArray;
+    let filteredResults = animalsArray;
 
     if (query.personalityTraits) {
         // Save personalityTraits as a dedicated array.
@@ -49,6 +56,35 @@ function findbyID(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body,animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname,'./data/animals.json'),  
+        JSON.stringify({animals:animalsArray},null,2)
+        );
+    return animal;
+}
+
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+}
+    if (!animal.diet || typeof animal.diet !== 'string'){
+    return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
+
+
 app.get('/api/animals', (req,res) => {
     let results = animals;
     if (req.query) {
@@ -66,7 +102,19 @@ app.get('/api/animals/:id', (req,res) => {
     }
 });
 
+app.post('/api/animals', (req,res) => {
+    //set id based on what the next index of the array will be
+    req.body.id=animals.length.toString();
 
+
+    //if any data in req.body is incorrect,send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body,animals);
+        res.json(animal);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
